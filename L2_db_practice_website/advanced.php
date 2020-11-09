0d5ae773e6b33935b82afe3f30a4f385419ecd14
SapE <60630338+kimj9537@users.noreply.github.com>
2020-11-10 11:59:49 +1300

    bonk

--- L2_db_practice_website/advanced.php
+++ L2_db_practice_website/advanced.php
@@ -6,17 +6,35 @@
     $genre = mysqli_real_escape_string($dbconnect, $_POST['genre']);
     $cost = mysqli_real_escape_string($dbconnect, $_POST['cost']);
 
+    // In App Purchases...
+    if (isset($_POST['in_app'])) {
+        $in_app = 0;
+        
+    }
 
+    else {
+        $in_app = 1;
+        
+    }
 
-        $find_sql = "SELECT * FROM `L2_prac_game_detail` JOIN `L2_prac_genre` ON (`L2_prac_game_detail`.`GenreID`=`L2_prac_genre`.`GenreID`) JOIN `L2_db_prac_developer` ON (`L2_prac_game_detail`.`DeveloperID`=`L2_db_prac_developer`.`DevID`) WHERE `Name` LIKE '%$app_name%' 
-        AND `Developer Name` LIKE '%$dev_name%'
-        AND `Genre` LIKE '%$genre%'
-        AND `Price` <= '$cost'
-        ";
+    // Ratings
+    $rating_more_less = mysqli_real_escape_string($dbconnect, $_POST['rate_more_less']);
+    $rating = mysqli_real_escape_string(dbconnect, $_POST['rating']);
 
-        $find_query = mysqli_query($dbconnect, $find_sql);
-        $find_rs = mysqli_fetch_assoc($find_query);
-        $count = mysqli_num_rows($find_query);
+    if () {}
+    elseif() {}
+    else {} // end rating if /elseif / else
+
+    $find_sql = "SELECT * FROM `L2_prac_game_detail` JOIN `L2_prac_genre` ON (`L2_prac_game_detail`.`GenreID`=`L2_prac_genre`.`GenreID`) JOIN `L2_db_prac_developer` ON (`L2_prac_game_detail`.`DeveloperID`=`L2_db_prac_developer`.`DevID`) WHERE `Name` LIKE '%$app_name%' 
+    AND `Developer Name` LIKE '%$dev_name%'
+    AND `Genre` LIKE '%$genre%'
+    AND `Price` <= '$cost'
+    AND (`Purchases?` = $in_app OR `Purchases?` = 0)
+    ";
+
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
 
 ?>
 
